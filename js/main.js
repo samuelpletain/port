e0dbb17a5b9cd66e2e987796195221fc0a1f4635
Samuel Pletain <samuel.pletain@gmail.com>
2022-09-12 10:15:59 -0600

    Portfolio setup

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const list = document.getElementById('list');
+const links = [
+  {
+    label: "Week 1 Notes",
+    url: "week1/index.html"
+  },
+  {
+    label: "Another item",
+    url: "week2/test.html"
+  }
+]
+
+function createListItems(list, items) {
+  items.forEach((item) => {
+    let li = document.createElement('li')
+    let a = document.createElement('a')
+    let linkText = document.createTextNode(item.label)
+    a.appendChild(linkText)
+    a.title = item.label
+    a.href = "./" + item.url
+    list.appendChild(li).appendChild(a)
+  })
+}
+
+createListItems(list, links)
